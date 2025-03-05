dc92d2422637d9c5acb220c6c859717a06b988b8
sludgeTanker <schm987@gmail.com>
2025-03-06 00:29:57 +0100

    groups

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
     
     print("Starting Asteroids!")
@@ -18,9 +25,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         
         #limit the framerate to 60 fps
